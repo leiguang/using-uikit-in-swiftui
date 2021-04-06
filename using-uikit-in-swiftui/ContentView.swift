3e1005d16b725d65af50c1086b5e3db68219fe46
Guang Lei <575403441@qq.com>
2021-04-06 13:02:30 +0800

    Initial Commit

---
+++ using-uikit-in-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  using-uikit-in-swiftui
+//
+//  Created by Guang Lei on 2021/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ using-uikit-in-swiftui/using_uikit_in_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  using_uikit_in_swiftuiApp.swift
+//  using-uikit-in-swiftui
+//
+//  Created by Guang Lei on 2021/4/6.
+//
+
+import SwiftUI
+
+@main
+struct using_uikit_in_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
